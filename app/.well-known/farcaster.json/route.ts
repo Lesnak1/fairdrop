9e4431fea3cd4a11fd0a822159401595228f0102
Lesnak1 <heleknax@users.noreply.github.com>
2025-12-06 18:08:41 +0300

    Add baseBuilder to manifest

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -5,5 +5,8 @@ export async function GET() {
     return NextResponse.json({
         frame: minikitConfig.frame,
         accountAssociation: minikitConfig.accountAssociation,
+        baseBuilder: {
+            ownerAddress: "0x5583101e8f0DcbAA99B58b0f141858166FE622ce"
+        }
     });
 }
